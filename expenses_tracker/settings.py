d54df195f4dc76f0bb8410316268438b1d02906c
Петър Иванов <81799675+peivanov@users.noreply.github.com>
2022-02-21 00:47:24 +0200

    Update settings.py

--- expenses_tracker/settings.py
+++ expenses_tracker/settings.py
@@ -7,8 +7,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$3nt1(u2(@x12-37=#ka-u@v%mher$z9_*-*y)!=i8@@4s#mwu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
